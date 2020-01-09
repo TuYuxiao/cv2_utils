acdf79ef4284fedd4d502da5f35fbbc405dd7e28
tuyuxiao <754738085@qq.com>
2020-01-09 13:53:31 +0800

    ~

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'cv2_utils',
+    version = '0.1.0',
+    description = 'Utils for cv based on OpenCV',
+    license = 'MIT License',
+    author = 'Tu Yuxiao',
+    author_email = '754738085@qq.com',
+    url = 'https://github.com/TuYuxiao/cv2_utils',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+    keywords = ['OpenCV'],
+)
